2e6e57d39d2dc810a442eac96953cc72a34b467e
mac <mac@macdemacbook-pro.local>
2016-10-25 09:09:01 +0800

    Initial Commit

---
+++ 014-- 联网检测/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  014-- 联网检测
+//
+//  Created by mac on 16/10/25.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 014-- 联网检测/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  014-- 联网检测
+//
+//  Created by mac on 16/10/25.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
